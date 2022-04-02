097427f8a4b091f92fd266da67ec23119b2bcd8d
Augusto Carmo <carmolim@gmail.com>
2022-04-01 21:44:59 -0300

    fixing indentation

--- components/Notifier.js
+++ components/Notifier.js
@@ -5,16 +5,16 @@ const axios = require('axios')
 
 exports.sendNotification = async ( msg ) => {
     
-	msg = encodeURI(msg)
+    msg = encodeURI(msg)
 
-	try {
+    try {
 
         return await axios.get('https://api.telegram.org/bot' + config.telegramToken + '/sendMessage?chat_id=' + config.telegramChatID + '&text=' + msg )
         
-	} catch ( error ) {
+    } catch ( error ) {
 
-		// log.error('Error sending the Telegram message');
+        // log.error('Error sending the Telegram message');
         throw Error( error );
 
-	}
+    }
 };
